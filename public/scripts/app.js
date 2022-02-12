5add8e7c0cff93ad12ef3d6c59e12aabc3327c17
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-12 23:47:15 +0000

    Test diaplaying categories on the main page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
 // Client facing scripts here
+$(() => {
+  $.get("/api/categories")
+  .then((data) => {
+    const categories = $("#categories");
+
+    for (let category of data.categories) {
+      const li = `<li>${category.name}</li>`
+      categories.append(li);
+    }
+
+  })
+});
