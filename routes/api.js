76ff07655a824710ffc1f300211db2ef88185d2f
Shauna Empringham <shaunathedead86@gmail.com>
2022-04-02 17:36:17 -0400

    updating more routing

--- routes/api.js
+++ routes/api.js
@@ -1,45 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const { encrypt, decrypt } = require('../encryption/encryption');
+const bcrypt = require("bcrypt");
+const { encrypt, decrypt } = require("../encryption/encryption");
+
+const targetURL = "ec2-3-217-251-77.compute-1.amazonaws.com";
 
 module.exports = (db) => {
-  router.get("/categories", (req, res) => {
-    db.query(`SELECT *
-    FROM categories;`)
-      .then(data => {
+  router.get(targetURL + "/categories", (req, res) => {
+    db.query(
+      `SELECT *
+    FROM categories;`
+    )
+      .then((data) => {
         const categories = data.rows;
         res.json({ categories });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.get("/credentials", (req, res) => {
-    db.query(`SELECT *
-    FROM credentials;`)
-      .then(data => {
+  router.get(targetURL + "/credentials", (req, res) => {
+    db.query(
+      `SELECT *
+    FROM credentials;`
+    )
+      .then((data) => {
         const credentials = data.rows;
         //decrypt password before returning to front-end
-        getConfig("ENCRYPTION_KEY")
-          .then(secretKey => {
-            for (const credential of credentials) {
-              credential.password = decrypt(credential.password, secretKey.value);
-            }
-            res.json({ credentials });
-          })
+        getConfig("ENCRYPTION_KEY").then((secretKey) => {
+          for (const credential of credentials) {
+            credential.password = decrypt(credential.password, secretKey.value);
+          }
+          res.json({ credentials });
+        });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.get("/credentials/id", (req, res) => {
+  router.get(targetURL + "/credentials/id", (req, res) => {
     const queryString = `SELECT *
     FROM credentials
     WHERE id = $1;`;
@@ -49,49 +50,44 @@ module.exports = (db) => {
       .then((data) => {
         const credentials = data.rows;
         //decrypt password before returning to front-end
-        getConfig("ENCRYPTION_KEY")
-          .then(secretKey => {
-            for (const credential of credentials) {
-              credential.password = decrypt(credential.password, secretKey.value);
-            }
-            res.send(credentials);
-          })
+        getConfig("ENCRYPTION_KEY").then((secretKey) => {
+          for (const credential of credentials) {
+            credential.password = decrypt(credential.password, secretKey.value);
+          }
+          res.send(credentials);
+        });
       })
       .catch((err) => {
         console.log(err);
-      })
+      });
   });
 
-  router.get("/credentials/delete", (req, res) => {
+  router.get(targetURL + "/credentials/delete", (req, res) => {
     const queryString = `
     DELETE FROM credentials
-    WHERE id = $1`
+    WHERE id = $1`;
     const queryParams = [req.query.passwordID];
 
     db.query(queryString, queryParams)
-      .then(data => {
+      .then((data) => {
         res.send(data);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.get("/categories/uncategorized", (req, res) => {
+  router.get(targetURL + "/categories/uncategorized", (req, res) => {
     db.query("SELECT * FROM categories WHERE name = 'Uncategorized'")
-      .then(data => {
+      .then((data) => {
         res.send(data.rows);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.post("/credentials/edit", (req, res) => {
+  router.post(targetURL + "/credentials/edit", (req, res) => {
     const queryString = `
     UPDATE credentials
     SET username = $1,
@@ -99,110 +95,107 @@ module.exports = (db) => {
     url = $3,
     name = $4,
     category_id = $5
-    WHERE id = $6;`
+    WHERE id = $6;`;
 
     // encrypt password before storing in DB
-    getConfig("ENCRYPTION_KEY")
-      .then(secretKey => {
-        const queryParams = [req.body.username, encrypt(req.body.password, secretKey.value), req.body.url, req.body.name, req.body.categoryId, req.body['password-id']];
-        db.query(queryString, queryParams)
-          .then(data => {
-            res.send(data);
-          })
-          .catch(err => {
-            res
-              .status(500)
-              .json({ error: err.message });
-          });
-      })
+    getConfig("ENCRYPTION_KEY").then((secretKey) => {
+      const queryParams = [
+        req.body.username,
+        encrypt(req.body.password, secretKey.value),
+        req.body.url,
+        req.body.name,
+        req.body.categoryId,
+        req.body["password-id"],
+      ];
+      db.query(queryString, queryParams)
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    });
   });
 
-  router.post("/credentials/move", (req, res) => {
+  router.post(targetURL + "/credentials/move", (req, res) => {
     const queryString = `
     UPDATE credentials
     SET category_id = $1
-    WHERE category_id = $2;`
+    WHERE category_id = $2;`;
     const queryParams = [req.body.newCategory, req.body.target];
 
     db.query(queryString, queryParams)
-      .then(data => {
+      .then((data) => {
         const credentials = data.rows;
         //decrypt password before returning to front-end
-        getConfig("ENCRYPTION_KEY")
-          .then(secretKey => {
-            for (const credential of credentials) {
-              credential.password = decrypt(credential.password, secretKey.value);
-            }
-            res.send(credentials);
-          })
+        getConfig("ENCRYPTION_KEY").then((secretKey) => {
+          for (const credential of credentials) {
+            credential.password = decrypt(credential.password, secretKey.value);
+          }
+          res.send(credentials);
+        });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.post("/categories/create", (req, res) => {
+  router.post(targetURL + "/categories/create", (req, res) => {
     const queryString = `INSERT INTO categories (name)
-    VALUES ($1)`
+    VALUES ($1)`;
     const queryParams = [req.body.name];
 
     db.query(queryString, queryParams)
-      .then(data => {
+      .then((data) => {
         res.send(data.rows);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.post("/categories/delete", (req, res) => {
+  router.post(targetURL + "/categories/delete", (req, res) => {
     const queryString = `
     DELETE FROM categories
-    WHERE id = $1`
+    WHERE id = $1`;
     const queryParams = [req.body.target];
 
     db.query(queryString, queryParams)
-      .then(data => {
+      .then((data) => {
         res.send(data.rows);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.post("/categories/edit", (req, res) => {
+  router.post(targetURL + "/categories/edit", (req, res) => {
     const queryString = `
     UPDATE categories
     SET name = $1
-    WHERE id = $2;`
+    WHERE id = $2;`;
     const queryParams = [req.body.oldName, req.body.newName];
 
     db.query(queryString, queryParams)
-      .then(data => {
+      .then((data) => {
         res.send(data);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.post("/login", (req, res) => {
-
+  router.post(targetURL + "/login", (req, res) => {
     const params = [req.body.username];
-    db.query(`
+    db.query(
+      `
     SELECT *
     FROM users
     WHERE username = $1
-    ;`, params)
-      .then(data => {
+    ;`,
+      params
+    )
+      .then((data) => {
         if (data.rows && data.rows.length > 0) {
           if (bcrypt.compareSync(req.body.password, data.rows[0].password)) {
             req.session["user_id"] = data.rows[0].id;
@@ -211,48 +204,41 @@ module.exports = (db) => {
           }
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err.message);
       });
   });
 
-  const getConfig = function(attr) {
-    return db.query(`SELECT * FROM configurations WHERE attribute = $1;`, [attr])
-      .then(data => {
+  const getConfig = function (attr) {
+    return db
+      .query(`SELECT * FROM configurations WHERE attribute = $1;`, [attr])
+      .then((data) => {
         if (data.rows.length > 0) {
           return data.rows[0];
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err.message);
       });
-  }
+  };
 
   // search for specific website_name and its password
-  router.post("/credentials/search", (req, res) => {
-
+  router.post(targetURL + "/credentials/search", (req, res) => {
     const queryString = `
   SELECT id, name, password
   FROM credentials
-  WHERE upper(name) LIKE $1;`
+  WHERE upper(name) LIKE $1;`;
 
     const queryParams = [`%${req.body.website.toUpperCase()}%`];
 
-
     db.query(queryString, queryParams)
-      .then(data => {
-
+      .then((data) => {
         const searchResult = data.rows;
         res.json({ searchResult });
-
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
-
-
   });
 
   return router;
--- routes/credentials.js
+++ routes/credentials.js
@@ -5,99 +5,92 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
+const targetURL = "ec2-3-217-251-77.compute-1.amazonaws.com";
+
+const express = require("express");
 const router = express.Router();
-const { encrypt, decrypt } = require('../encryption/encryption');
+const { encrypt, decrypt } = require("../encryption/encryption");
 module.exports = (db) => {
   // GET /credentials - get all credentials
-  router.get("/", (req, res) => {
+  router.get(targetURL + "/", (req, res) => {
     db.query(`SELECT * FROM credentials;`)
-      .then(data => {
+      .then((data) => {
         const credentials = data.rows;
         //decrypt password before returning to front-end
-        getConfig("ENCRYPTION_KEY")
-          .then(secretKey => {
-            for (const credential of credentials) {
-              credential.password = decrypt(credential.password, secretKey.value);
-            }
-          })
+        getConfig("ENCRYPTION_KEY").then((secretKey) => {
+          for (const credential of credentials) {
+            credential.password = decrypt(credential.password, secretKey.value);
+          }
+        });
         res.json({ credentials });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
   // GET only IDs from /credentials
-  router.get("/id", (req, res) => {
+  router.get(targetURL + "/id", (req, res) => {
     db.query(`SELECT id FROM credentials;`)
-      .then(data => {
+      .then((data) => {
         const credentials = data.rows;
         res.json({ credentials });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
   // POST /credentials - create a new credential
-  router.post("/", (req, res) => {
-
+  router.post(targetURL + "/", (req, res) => {
     // get user_id and organization_id from session
     const userId = req.currentUser || 2;
     const orgId = req.currentOrg || 2;
 
     //prepare query for insert new credential
-    getConfig("ENCRYPTION_KEY")
-      .then(secretKey => {
-        const insertParams = [
-          req.body.username,
-          encrypt(req.body.password, secretKey.value),
-          req.body.url,
-          req.body.name,
-          userId,
-          orgId,
-          req.body.categoryId,
-          req.body.logo_url ? req.body.logo_url : "../images/passwordkeepr.png"
-        ];
+    getConfig("ENCRYPTION_KEY").then((secretKey) => {
+      const insertParams = [
+        req.body.username,
+        encrypt(req.body.password, secretKey.value),
+        req.body.url,
+        req.body.name,
+        userId,
+        orgId,
+        req.body.categoryId,
+        req.body.logo_url ? req.body.logo_url : "../images/passwordkeepr.png",
+      ];
 
-        const insertQueryString = `
+      const insertQueryString = `
         INSERT INTO credentials (username, password, url, name, creator_id, organization_id, category_id, logo_url)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
         RETURNING *;
         `;
 
-        db.query(insertQueryString, insertParams)
-          .then(data => {
-            credential = data.rows[0];
-            templateVars = { credential }
-            res.render("index", templateVars);
-          })
-          .catch(err => {
-            console.log(err)
-            res
-              .status(500)
-              .json({ error: err.message });
-          });
-      })
+      db.query(insertQueryString, insertParams)
+        .then((data) => {
+          credential = data.rows[0];
+          templateVars = { credential };
+          res.render("index", templateVars);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({ error: err.message });
+        });
+    });
+  });
 
-  })
-
-  const getConfig = function(attr) {
-    return db.query(`SELECT * FROM configurations WHERE attribute = $1;`, [attr])
-      .then(data => {
+  const getConfig = function (attr) {
+    return db
+      .query(`SELECT * FROM configurations WHERE attribute = $1;`, [attr])
+      .then((data) => {
         if (data.rows.length > 0) {
           return data.rows[0];
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err.message);
       });
-  }
+  };
 
   return router;
-}
+};
